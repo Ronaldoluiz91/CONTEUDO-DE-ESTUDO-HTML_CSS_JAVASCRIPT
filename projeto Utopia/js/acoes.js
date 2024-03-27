1ec5c709d44fcd20240e08201b1339cfb236e6da
Ronaldoluiz91 <ronaldo.lsilva6@senacsp.edu.br>
2024-03-27 02:41:36 -0300

    finalizado menu hamburguer projeto utopia

--- projeto Utopia/js/acoes.js
+++ projeto Utopia/js/acoes.js
@@ -1,14 +1,36 @@
 //alert('funcionou')
 var btnmostrar = document.getElementById('iconMenu'); // icone  do menu
-var menus = document.getElementById('itens'); // menu a ser ocultado e/ou mostrado
+var menu = document.getElementById('itens'); // menu a ser ocultado e/ou mostrado
+var logo = document.getElementById('logotipo'); // logotipo do site
+var btnOcultar = document.getElementById('iconClose') // icone fechar do menu
+
 
 btnmostrar.addEventListener('click', function() {
-    if(menus.style.display === 'block')
+    if(menu.style.display === 'block')
     {
-        menus.style.display = 'none';
+        menu.style.display = 'none';
     }
      else{
-        menus.style.display = 'block';
+        menu.style.display = 'block';
     }
 
 })
+
+btnmostrar.addEventListener('click', function() {
+    if(logo.style.display === 'none')
+    {
+        logo.style.display = 'block';
+    }
+     else{
+        logo.style.display = 'none';
+    
+    }
+
+})
+
+btnOcultar.addEventListener('click', function() {
+ menu.style.display = 'none';
+ logo.style.display = 'block'
+})
+
+
